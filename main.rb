e68c57c777618f68b90760cc4d88d90f256a3646
yous <yousbe@gmail.com>
2012-01-13 02:20:55 +0900

    displayToon을 HTTPServlet으로 제작, cgi를 rb로 변경

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require "webrick"
+require "./html/displayToon"
 
 server = WEBrick::HTTPServer.new :DocumentRoot => File.join(Dir.pwd, "/html"), :Port => 8888
+server.mount "displayToon.cgi", DisplayToon
 
 trap("INT") { server.shutdown }
 server.start
