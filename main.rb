084e17f0cc4ea55ec178e7455b00552481e31a6e
yous <yousbe@gmail.com>
2012-01-13 03:02:42 +0900

    rb 파일은 FileHandler에서 제외

--- main.rb
+++ main.rb
@@ -3,7 +3,9 @@ require "./html/displayToon"
 require "./html/getNum"
 require "./html/getOtherToon"
 
-server = WEBrick::HTTPServer.new :DocumentRoot => File.join(Dir.pwd, "/html"), :Port => 8888
+server = WEBrick::HTTPServer.new :DocumentRoot => File.join(Dir.pwd, "/html"), :Port => 8888,
+  :DocumentRootOptions => {:NondisclosureName => "*.rb"}
+
 server.mount "/displayToon", DisplayToon
 server.mount "/getNum", GetNum
 server.mount "/getOtherToon", GetOtherToon
