b687a7091d2738ca98976210f174fba6f5e5e514
yous <yousbe@gmail.com>
2012-01-13 02:33:19 +0900

    getOtherToon을 HTTPServlet으로 작성, cgi를 rb로 변경

--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
 require "webrick"
 require "./html/displayToon"
 require "./html/getNum"
+require "./html/getOtherToon"
 
 server = WEBrick::HTTPServer.new :DocumentRoot => File.join(Dir.pwd, "/html"), :Port => 8888
 server.mount "/displayToon.cgi", DisplayToon
 server.mount "/getNum.cgi", GetNum
+server.mount "/getOtherToon.cgi", GetOtherToon
 
 trap("INT") { server.shutdown }
 server.start
