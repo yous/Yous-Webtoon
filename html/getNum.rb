93fef551f36a2252f31fc42066a9e02990368b4d
yous <yousbe@gmail.com>
2012-01-13 02:27:06 +0900

    getNum을 HTTPServlet으로 작성, cgi를 rb로 변경

---
+++ html/getNum.rb
@@ -0,0 +1,67 @@
+require "rubygems"
+require "webrick"
+require "mechanize"
+
+class GetNum < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(req, res)
+    res.status = 200
+    res["Content-Type"] = "text/html; charset=utf-8"
+
+    site = req.query["site"]
+    id = req.query["id"]
+
+    a = Mechanize.new
+
+    # Naver 웹툰
+    if site == "naver"
+      str = ""
+      resp = a.get "http://comic.naver.com/webtoon/detail.nhn?titleId=#{id}"
+
+      resp.search('//div[@id="header"]/div[@id="submenu"]/ul[@class="submenu"]/li').each do |v|
+        v.search('a[@class="current"]').each do |e|
+          str << case e.attributes["href"].value
+          when "/webtoon/weekday.nhn" then "n "
+          when "/webtoon/finish.nhn" then "y "
+          else "x "
+          end
+        end
+      end
+      resp.search('//div[@class="btn_area"]').each do |v|
+        v.search('span[@class="pre"]/a').each do |e|
+          if e.attributes["href"].value =~ /\/webtoon\/detail\.nhn\?titleId=\d+&seq=(\d+)/
+            res.body = (str << "#{$1.to_i + 1}")
+            exit
+          end
+        end
+        res.body = (str << "1")
+      end
+
+      # Daum 웹툰
+    elsif site == "daum"
+      str = ""
+      str_writer = []
+      str_toonInfo = ""
+      str_finish = ""
+      resp = a.get "http://cartoon.media.daum.net/webtoon/view/#{id}"
+
+      resp.search('//div[@id="daumContent"]/div[@id="cMain"]').each do |r|
+        r.search('div[@id="mCenter"]/div[@class="area_toon_info"]').each do |v|
+          str_writer.push(v.at('div[@class="wrap_cont"]/dl[1]/dd/a').inner_html.strip)
+          str_toonInfo = v.at('div[@class="wrap_more"]/dl[@class="list_intro"]/dd').inner_html.strip
+        end
+        r.at('div[@id="mCenter"]/script').inner_html.strip.split(";").map(&:strip).
+          find_all {|v| v =~ /data1\.push\([\w\W]*\)/}.
+          map {|v|
+          if v =~ /data1\.push\(\s*\{\s*img\s*:\s*"[\w\W]*"\s*,\s*title\s*:\s*"[\w\W]*"\s*,\s*shortTitle\s*:\s*"[\w\W]*"\s*,\s*url\s*:\s*"\/webtoon\/viewer\/(\d+)"\s*,\s*date\s*:\s*"([\w\W]*)"\s*,\s*price\s*:\s*"[\w\W]*"\s*,\s*finishYn\s*:\s*"([\w\W]*)"\s*,\s*payYn\s*:\s*"[\w\W]*"\s*\}\s*\)/
+            str_finish = "y " if $3 == "Y" and str_finish == ""
+            {"num" => $1, "date" => $2, "finish" => $3}
+          end
+        }.
+          reverse.
+          each {|v| str << "#{v["num"]},#{v["date"]} " }
+      end
+
+      res.body = ((str_finish == "") ? "n " : str_finish) + str[0...-1] + "\n" + str_writer.join(" / ") + "\n" + str_toonInfo
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 require "webrick"
 require "./html/displayToon"
+require "./html/getNum"
 
 server = WEBrick::HTTPServer.new :DocumentRoot => File.join(Dir.pwd, "/html"), :Port => 8888
-server.mount "displayToon.cgi", DisplayToon
+server.mount "/displayToon.cgi", DisplayToon
+server.mount "/getNum.cgi", GetNum
 
 trap("INT") { server.shutdown }
 server.start
