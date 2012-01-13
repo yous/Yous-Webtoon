e6aa6a502ac99be9621952ada23c6ba226214275
yous <yousbe@gmail.com>
2012-01-13 22:59:56 +0900

    WEBrick reverse lookup 중지

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ require "./html/displayToon"
 require "./html/getNum"
 require "./html/getOtherToon"
 
+BasicSocket.do_not_reverse_lookup = true
+
 server = WEBrick::HTTPServer.new :DocumentRoot => File.join(Dir.pwd, "/html"), :Port => 8888,
   :DocumentRootOptions => {:NondisclosureName => ["*.rb"]}
 
