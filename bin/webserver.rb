0a52f7ef3026735d6e82163dd3ccdf65f1bcfae6
yous <yousbe@gmail.com>
2012-01-14 22:43:17 +0900

    Log 파일 형식 설정

--- bin/webserver.rb
+++ bin/webserver.rb
@@ -5,7 +5,8 @@ require "getOtherToon"
 
 BasicSocket.do_not_reverse_lookup = true
 
-server = WEBrick::HTTPServer.new :DocumentRoot => File.join(File.dirname(__FILE__), "/../html"), :BindAddress => "0.0.0.0", :Port => 8888
+server = WEBrick::HTTPServer.new :DocumentRoot => File.join(File.dirname(__FILE__), "/../html"), :BindAddress => "0.0.0.0", :Port => 8888,
+  :AccessLog => [[File.open(File.join(File.dirname(__FILE__), "/../Log"), "a"), WEBrick::AccessLog::COMBINED_LOG_FORMAT]]
 
 server.mount "/displayToon", DisplayToon
 server.mount "/getNum", GetNum
