a199fbf78a14dc830154b560ee7c9ba29c91e150
yous <yousbe@gmail.com>
2012-01-14 15:43:38 +0900

    더 이상 필요 없어진 DocumentRootOptions 제거

--- bin/webserver.rb
+++ bin/webserver.rb
@@ -5,8 +5,7 @@ require "getOtherToon"
 
 BasicSocket.do_not_reverse_lookup = true
 
-server = WEBrick::HTTPServer.new :DocumentRoot => File.join(File.dirname(__FILE__), "/../html"), :BindAddress => "0.0.0.0", :Port => 8888,
-  :DocumentRootOptions => {:NondisclosureName => ["*.rb"]}
+server = WEBrick::HTTPServer.new :DocumentRoot => File.join(File.dirname(__FILE__), "/../html"), :BindAddress => "0.0.0.0", :Port => 8888
 
 server.mount "/displayToon", DisplayToon
 server.mount "/getNum", GetNum
