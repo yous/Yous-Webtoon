7e5af7b2aaa04ba3adbde31c9e9913f99e2315d9
yous <yousbe@gmail.com>
2012-01-14 15:41:36 +0900

    Address already in use 에러 Troubleshooting

--- bin/webserver.rb
+++ bin/webserver.rb
@@ -5,7 +5,7 @@ require "getOtherToon"
 
 BasicSocket.do_not_reverse_lookup = true
 
-server = WEBrick::HTTPServer.new :DocumentRoot => File.join(File.dirname(__FILE__), "/../html"), :Port => 8888,
+server = WEBrick::HTTPServer.new :DocumentRoot => File.join(File.dirname(__FILE__), "/../html"), :BindAddress => "0.0.0.0", :Port => 8888,
   :DocumentRootOptions => {:NondisclosureName => ["*.rb"]}
 
 server.mount "/displayToon", DisplayToon
