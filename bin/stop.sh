c5be8690edd7ddf54d685e04ad4b745938fd1e16
yous <yousbe@gmail.com>
2012-02-05 01:32:15 +0900

    stop.sh 오류 출력하지 않음

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps x | grep "ruby -Ilib bin/webserver.rb" | grep -v "grep" | grep -o -E "^\ *[0-9]+" | grep -o -E "[0-9]+" | xargs kill -9; rm sess/*
+ps x | grep "ruby -Ilib bin/webserver.rb" | grep -v "grep" | grep -o -E "^\ *[0-9]+" | grep -o -E "[0-9]+" | xargs kill -9 2>/dev/null; rm sess/* 2>/dev/null
