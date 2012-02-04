f8310597c21890a082adc6e2599d69f6bee939bf
yous <yousbe@gmail.com>
2012-02-05 01:31:08 +0900

    stop.sh 실행 시 ruby를 모두 kill 하는 오류 수정

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -C "ruby -Ilib bin/webserver.rb" -o pid= | xargs kill -9; rm sess/*
+ps x | grep "ruby -Ilib bin/webserver.rb" | grep -v "grep" | grep -o -E "^\ *[0-9]+" | grep -o -E "[0-9]+" | xargs kill -9; rm sess/*
