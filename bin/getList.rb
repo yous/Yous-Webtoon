74c0d44dea4b580c76d5e8fda5e0bb0c7c388e47
ChaYoung You <yousbe@gmail.com>
2013-01-31 15:01:41 +0900

    getList.rb 로그 에러 형식 수정

--- bin/getList.rb
+++ bin/getList.rb
@@ -19,7 +19,7 @@ while true
         site.getList :all => true
         logger.info "#{site.site} ALL"
       rescue Exception => e
-        logger.error "#{site.site} ALL\n#{e.backtrace[0]}: #{e.message} (#{e.class})\n#{e.backtrace.drop(1).map {|msg| "\tfrom #{msg}"}}"
+        logger.error "#{site.site} ALL\n#{e.backtrace[0]}: #{e.message} (#{e.class})\n#{e.backtrace.drop(1).map {|msg| "\tfrom #{msg}"}.join("\n")}"
       end
     end
   else
@@ -28,7 +28,7 @@ while true
         site.getList :all => false
         logger.info site.site
       rescue Exception => e
-        logger.error "#{site.site}\n#{e.backtrace[0]}: #{e.message} (#{e.class})\n#{e.backtrace.drop(1).map {|msg| "\tfrom #{msg}"}}"
+        logger.error "#{site.site}\n#{e.backtrace[0]}: #{e.message} (#{e.class})\n#{e.backtrace.drop(1).map {|msg| "\tfrom #{msg}"}.join("\n")}"
       end
     end
   end
